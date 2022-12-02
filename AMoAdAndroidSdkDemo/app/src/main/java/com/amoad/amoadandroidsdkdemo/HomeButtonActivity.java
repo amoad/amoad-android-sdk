b66da6026d117ff2562b53f2108fa45385ae3c71
Shuma Yoshioka <s.yoshioka@p.cartahd.com>
2022-12-02 12:54:03 +0900

    Migrate to AndroidX

--- AMoAdAndroidSdkDemo/app/src/main/java/com/amoad/amoadandroidsdkdemo/HomeButtonActivity.java
+++ AMoAdAndroidSdkDemo/app/src/main/java/com/amoad/amoadandroidsdkdemo/HomeButtonActivity.java
@@ -1,8 +1,8 @@
 package com.amoad.amoadandroidsdkdemo;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.MenuItem;
 
 public abstract class HomeButtonActivity extends AppCompatActivity {
